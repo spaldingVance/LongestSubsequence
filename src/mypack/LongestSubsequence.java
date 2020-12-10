68919b94d3bfed907f98a2913b75da954fb5f853
Spalding Vance <sclarkvance@gmail.com>
2020-12-10 15:02:42 -0500

    fixed formatting

--- src/mypack/LongestSubsequence.java
+++ src/mypack/LongestSubsequence.java
@@ -20,23 +20,26 @@ public class LongestSubsequence {
 	}
 	
 	static int findLongestSubsequence(int numArr[], int length) {
-		System.out.println("Called");
+		//base case
 		if (length == 1) {
-			System.out.println("Length is 1, returning");
 			return 1;
 		}
 		
 		int previousMaxLength = 1;
+		
 		for(int i = 1; i < length; i++) {
+			
 			int currentMaxLength = findLongestSubsequence(numArr, i);
 			
 			if (numArr[i-1] < numArr[length-1] && currentMaxLength + 1 > previousMaxLength) {
+				
 				
                 previousMaxLength = currentMaxLength + 1; 
             }
 			
 		}
 		if (maxLength < previousMaxLength) {
+			
 			maxLength = previousMaxLength; 
 		}
 

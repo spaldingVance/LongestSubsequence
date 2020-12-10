95b4ef82667a34de104ba8fe6f9156b5679ec271
Spalding Vance <sclarkvance@gmail.com>
2020-12-10 10:50:49 -0500

    finished project

--- src/mypack/LongestSubsequence.java
+++ src/mypack/LongestSubsequence.java
@@ -1,10 +1,46 @@
 package mypack;
 
+import java.util.Arrays; 
+
 public class LongestSubsequence {
 
+	
+	static int maxLength;
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numArr[] = { 6, 7, 2, 3, 5, 4}; 
+		
+		
+		System.out.println(Arrays.toString(numArr));
+		int length = numArr.length;
+		int result = findLongestSubsequence(numArr, length);
+		System.out.println("The longest subsequence is of length: " + result );
 
+	}
+	
+	static int findLongestSubsequence(int numArr[], int length) {
+		System.out.println("Called");
+		if (length == 1) {
+			System.out.println("Length is 1, returning");
+			return 1;
+		}
+		
+		int previousMaxLength = 1;
+		for(int i = 1; i < length; i++) {
+			int currentMaxLength = findLongestSubsequence(numArr, i);
+			
+			if (numArr[i-1] < numArr[length-1] && currentMaxLength + 1 > previousMaxLength) {
+				
+                previousMaxLength = currentMaxLength + 1; 
+            }
+			
+		}
+		if (maxLength < previousMaxLength) {
+			maxLength = previousMaxLength; 
+		}
+
+	        return previousMaxLength; 
 	}
 
 }
